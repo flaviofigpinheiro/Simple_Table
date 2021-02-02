f0da34b91d72c13ea7e2380568907a09b720e9dd
Flavio <Flaviofigpinheiro@gmail.com>
2021-02-02 13:02:44 +0000

    Initial commit

---
+++ src/SimpleTable.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class SimpleTable extends JPanel {
+
+    private static boolean DEBUG = true;
+
+
+    public static void main(String[] args) {
+        if (DEBUG) {
+            System.out.println("Hello World ! Simple Table");
+
+
+        }
+
+    }
+
+}
